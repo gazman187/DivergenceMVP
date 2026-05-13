a788f46cd5fbe0743ed6c25e912e56d31cc49a66
stealthgray <garyhaslett@gmail.com>
2026-05-13 17:45:02 +0100

    Add hallway trigger zone

---
+++ scripts/world/TriggerZone.gd
@@ -0,0 +1,15 @@
+extends Interactable
+class_name TriggerZone
+
+@export var trigger_action: String = "hallway_collapse"
+
+
+func interact(player_id: String) -> bool:
+	if not super.interact(player_id):
+		return false
+
+	if trigger_action == "hallway_collapse":
+		return SceneRouter.attempt_hallway_cross(player_id)
+
+	_emit_prompt("This trigger is only a greybox placeholder.")
+	return false
